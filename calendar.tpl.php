50185fcfd3886220336cbcbf18c9c0b3754a5087
Dan Rogers <dan.rogers@idfive.com>
2016-01-25 07:52:02 -0500

    Initial commit for sandbox from Howard project

---
+++ calendar.tpl.php
@@ -0,0 +1,7 @@
+<div ng-app="calendar" class="idfive-calendar">
+  <div ng-view></div>
+</div>
+<script>
+  window.siteId = Drupal.settings.idfiveCalendarEmbed.siteId;
+  window.siteUrl = Drupal.settings.idfiveCalendarEmbed.siteUrl + 'api';
+</script>
